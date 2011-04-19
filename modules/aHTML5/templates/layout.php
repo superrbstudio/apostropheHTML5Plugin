a62e713884e827a70e0c4e917440e3a331e4859c
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-04-19 16:46:21 +0000

    added a-engine check to body class
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheHTML5Plugin/trunk@3562 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aHTML5/templates/layout.php
+++ modules/aHTML5/templates/layout.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <?php use_helper('a') ?>
 <?php $page = aTools::getCurrentNonAdminPage() ?>
+<?php $realPage = aTools::getCurrentPage() ?>
 <?php $root = aPageTable::retrieveBySlug('/') ?>
 <!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
 <!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
@@ -46,8 +47,7 @@
 <?php // a-body-class allows you to set a class for the body element from a template ?>
 <?php // body_class is preserved here for backwards compatibility ?>
 
-<body class="<?php if (has_slot('a-body-class')): ?> <?php include_slot('a-body-class') ?><?php endif ?><?php if (has_slot('body_class')): ?> <?php include_slot('body_class') ?><?php endif ?><?php echo ($sf_user->isAuthenticated()) ? ' logged-in':' logged-out' ?><?php echo (sfConfig::get('app_a_js_debug', false)) ? ' js-debug':'' ?>">
-
+<body class="<?php if (has_slot('a-body-class')): ?> <?php include_slot('a-body-class') ?><?php endif ?><?php if (has_slot('body_class')): ?> <?php include_slot('body_class') ?><?php endif ?><?php echo ($sf_user->isAuthenticated()) ? ' logged-in':' logged-out' ?><?php echo (sfConfig::get('app_a_js_debug', false)) ? ' js-debug':'' ?><?php if ($realPage && !is_null($realPage['engine'])): ?> a-engine<?php endif ?>">
 	<?php include_partial('a/doNotEdit') ?>
   <?php include_partial('a/globalTools') ?>
 
