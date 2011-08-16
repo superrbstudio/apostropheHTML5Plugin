71006e827f592178ac953af41fa9ce1d35f50a14
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-08-16 20:38:01 +0000

    aHTML5 action class and base class no longer have potentially conflicting names
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheHTML5Plugin/trunk@4061 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aHTML5/actions/actions.class.php
+++ modules/aHTML5/actions/actions.class.php
@@ -10,6 +10,6 @@ require_once dirname(__FILE__).'/../lib/BaseaActions.class.php';
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 12534 2008-11-01 13:38:27Z Kris.Wallsmith $
  */
-class aActions extends BaseaActions
+class aHTML5Actions extends BaseaHTML5Actions
 {
 }
--- modules/aHTML5/lib/BaseaHTML5Actions.class.php
+++ modules/aHTML5/lib/BaseaHTML5Actions.class.php
@@ -8,6 +8,6 @@
  * @author      Your name here
  * @version     SVN: $Id: BaseActions.class.php 12534 2008-11-01 13:38:27Z Kris.Wallsmith $
  */
-abstract class BaseaActions extends sfActions
+abstract class BaseaHTML5Actions extends sfActions
 {
 }
